e73a5b2936f9f574e22b28048f320b98fffbb3f5
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-08-31 15:48:21 -0300

    main: feat (lib) - Adição das interfaces "GalleryImage" e "MovieDetail" e implementação de dados de filmes na nova estrutura

---
+++ yuffie-cinematic/lib/details.ts
@@ -0,0 +1,90 @@
+export interface GalleryImage {
+  id: string;
+  title: string;
+  src: string;
+  description?: string;
+  photoCredit?: string;
+  sourceLink?: string;
+  people?: string[];
+}
+
+export interface MovieDetail {
+  id: string;
+  title: string;
+  synopsis: string;
+  year: number;
+  director: string;
+  cover: string;
+  cast: string[];
+  carouselImages: string[];
+  rating: number;
+  genre?: string[];
+  duration?: number;
+  galleryImages?: GalleryImage[];
+}
+
+export const movies: MovieDetail[] = [
+  {
+    id: "a-time-to-kill",
+    title: "A Time to Kill",
+    synopsis:
+      "Um jovem advogado defende um homem negro acusado de assassinar dois homens brancos que estupraram sua filha de 10 anos, provocando o renascimento da Ku Klux Klan.",
+    year: 1996,
+    director: "Joel Schumacher",
+    cover: "/assets/images/movies/a-time-to-kill-1996.jpg",
+    cast: [
+      "Matthew McConaughey",
+      "Sandra Bullock",
+      "Samuel L. Jackson",
+      "Kevin Spacey",
+    ],
+    carouselImages: ["/assets/images/a-time-to-kill-banner.jpg"],
+    rating: 4,
+    genre: ["Drama", "Crime", "Thriller"],
+    duration: 149,
+    galleryImages: [
+      {
+        id: "1",
+        title: "Cena do Tribunal",
+        src: "/assets/images/gallery/a-time-to-kill/01.png",
+        description: "Jake Brigance defendendo Carl Lee Hailey no tribunal",
+        photoCredit: "Warner Bros Pictures",
+        sourceLink: "https://warnerbros.com",
+        people: [
+          "Matthew McConaughey (Jake Brigance)",
+          "Samuel L. Jackson (Carl Lee Hailey)",
+          "Kevin Spacey (Rufus Buckley)",
+        ],
+      },
+      {
+        id: "2",
+        title: "Bastidores da Produção",
+        src: "/assets/images/gallery/a-time-to-kill/02.png",
+        description: "Joel Schumacher dirigindo a cena do tribunal",
+        photoCredit: "Warner Bros Pictures",
+        sourceLink: "https://warnerbros.com",
+        people: [
+          "Joel Schumacher (Diretor)",
+          "Matthew McConaughey (Jake Brigance)",
+          "Peter Menzies Jr. (Diretor de Fotografia)",
+        ],
+      },
+    ],
+  },
+
+  {
+    id: "the-godfather",
+    title: "O Poderoso Chefão",
+    synopsis:
+      "A história da família mafiosa Corleone e seu patriarca Vito Corleone.",
+    year: 1972,
+    director: "Francis Ford Coppola",
+    cover: "/assets/images/movies/the-godfather-1972.jpg",
+    cast: ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+    carouselImages: ["/assets/images/movies/the-godfather-1972.jpg"],
+    rating: 5,
+    genre: ["Crime", "Drama"],
+    duration: 175,
+    galleryImages: [],
+  },
+];
--- yuffie-cinematic/lib/items.ts
+++ yuffie-cinematic/lib/items.ts
@@ -1,237 +1,280 @@
 export type Item = {
+  id: string;
   title: string;
   poster: string;
   video?: string | null;
-  detail?: string;
   category: "filme" | "serie" | "anime";
 };
 
 export const items: Item[] = [
   {
+    id: "a-time-to-kill",
     title: "Tempo de Matar",
     poster: "/assets/images/movies/a-time-to-kill-1996.jpg",
     video: "/assets/videos/a-time-to-kill-trailer.mp4",
-    detail: "/movie-description",
     category: "filme",
   },
   {
+    id: "the-x-files",
     title: "Arquivo X",
     poster: "/assets/images/series/the-x-files-1993.jpg",
     video: "/assets/videos/the-x-files-trailer.mp4",
-    detail: "/serie-description",
     category: "serie",
   },
   {
+    id: "the-godfather",
     title: "O Poderoso Chefão",
     poster: "/assets/images/movies/the-godfather-1972.jpg",
     category: "filme",
   },
   {
+    id: "fallen-angels",
     title: "Anjos Caídos",
     poster: "/assets/images/movies/fallen-angels-1995.jpg",
     category: "filme",
   },
   {
+    id: "amadeus",
     title: "Amadeus",
     poster: "/assets/images/movies/amadeus-1984.jpg",
     category: "filme",
   },
   {
+    id: "john-wick-2",
     title: "John Wick: Capítulo 2",
     poster: "/assets/images/movies/john-wick-chapter-2-2017.jpg",
     category: "filme",
   },
   {
+    id: "ip-man",
     title: "Ip Man",
     poster: "/assets/images/movies/ip-man-2008.jpg",
     category: "filme",
   },
   {
+    id: "shawshank-redemption",
     title: "Um Sonho de Liberdade",
     poster: "/assets/images/movies/the-shawshank-redemption-1994.jpg",
     category: "filme",
   },
   {
+    id: "butterfly-effect",
     title: "Efeito Borboleta",
     poster: "/assets/images/movies/the-butterfly-effect-2004.jpg",
     category: "filme",
   },
   {
+    id: "imitation-game",
     title: "O Jogo da Imitação",
     poster: "/assets/images/movies/the-imitation-game-2014.jpg",
     category: "filme",
   },
   {
+    id: "parasite",
     title: "Parasita",
     poster: "/assets/images/movies/parasite-2019.jpg",
     category: "filme",
   },
   {
+    id: "back-to-the-future",
     title: "De Volta para o Futuro",
     poster: "/assets/images/movies/back-to-the-future-1985.jpg",
     category: "filme",
   },
   {
+    id: "beverly-hills-cop",
     title: "Um Tira da Pesada",
     poster: "/assets/images/movies/beverly-hills-cop-1984.jpg",
     category: "filme",
   },
   {
+    id: "escape-from-new-york",
     title: "Fuga de Nova York",
     poster: "/assets/images/movies/escape-from-new-york-1981.jpg",
     category: "filme",
   },
   {
+    id: "road-house",
     title: "Matador de Aluguel",
     poster: "/assets/images/movies/road-house-1989.jpg",
     category: "filme",
   },
   {
+    id: "they-live",
     title: "Eles Vivem",
     poster: "/assets/images/movies/they-live-1988.jpg",
     category: "filme",
   },
   {
+    id: "13-reasons-why",
     title: "13 Reasons Why",
     poster: "/assets/images/series/13-reasons-why-2017.jpg",
     category: "serie",
   },
   {
+    id: "breaking-bad",
     title: "Breaking Bad",
     poster: "/assets/images/series/breaking-bad-2008.png",
     category: "serie",
   },
   {
+    id: "cobra-kai",
     title: "Cobra Kai",
     poster: "/assets/images/series/cobra-kai-2018.jpg",
     category: "serie",
   },
   {
+    id: "disenchantment",
     title: "Desencanto",
     poster: "/assets/images/series/desencanto-2018.jpg",
     category: "serie",
   },
   {
+    id: "futurama",
     title: "Futurama",
     poster: "/assets/images/series/futurama-1999.jpg",
     category: "serie",
   },
   {
+    id: "icarly",
     title: "iCarly",
     poster: "/assets/images/series/icarly-2007.jpg",
     category: "serie",
   },
   {
+    id: "rick-and-morty",
     title: "Rick and Morty",
     poster: "/assets/images/series/rick-and-morty-2013.png",
     category: "serie",
   },
   {
+    id: "smiling-friends",
     title: "Smiling Friends",
     poster: "/assets/images/series/smiling-friends-2022.jpg",
     category: "serie",
   },
   {
+    id: "squid-game",
     title: "Squid Game",
     poster: "/assets/images/series/squid-game-2021.jpg",
     category: "serie",
   },
   {
+    id: "stranger-things",
     title: "Stranger Things",
     poster: "/assets/images/series/stranger-things-2016.jpg",
     category: "serie",
   },
   {
+    id: "supernatural",
     title: "Supernatural",
     poster: "/assets/images/series/supernatural-2005.jpg",
     category: "serie",
   },
   {
+    id: "the-boys",
     title: "The Boys",
     poster: "/assets/images/series/the-boys-2019.jpg",
     category: "serie",
   },
   {
+    id: "the-simpsons",
     title: "The Simpsons",
     poster: "/assets/images/series/the-simpsons-1989.jpg",
     category: "serie",
   },
   {
+    id: "todo-mundo-odeia-o-chris",
     title: "Todo Mundo Odeia o Chris",
     poster: "/assets/images/series/todo-mundo-odeia-o-chris-2005.jpg",
     category: "serie",
   },
   {
+    id: "barakamon",
     title: "Barakamon",
     poster: "/assets/images/animes/barakamon-2014.jpg",
     category: "anime",
   },
   {
+    id: "berserk",
     title: "Berserk",
     poster: "/assets/images/animes/berserk-1997.jpg",
     category: "anime",
   },
   {
+    id: "bleach",
     title: "Bleach",
     poster: "/assets/images/animes/bleach-2004.jpg",
     category: "anime",
   },
   {
+    id: "cowboy-bebop",
     title: "Cowboy Bebop",
     poster: "/assets/images/animes/cowboy-bebop-1998.jpg",
     category: "anime",
   },
   {
+    id: "durarara",
     title: "Durarara!!",
     poster: "/assets/images/animes/durarara-2010.jpg",
     category: "anime",
   },
   {
+    id: "fairy-tail",
     title: "Fairy Tail",
     poster: "/assets/images/animes/fairy-tail-2014.jpg",
     category: "anime",
   },
   {
+    id: "fullmetal-alchemist",
     title: "Fullmetal Alchemist",
     poster: "/assets/images/animes/fullmetal-alchemist-2003.jpg",
     category: "anime",
   },
   {
+    id: "gintama",
     title: "Gintama",
     poster: "/assets/images/animes/gintama-2006.jpg",
     category: "anime",
   },
   {
+    id: "great-teacher-onizuka",
     title: "Great Teacher Onizuka",
     poster: "/assets/images/animes/great-teacher-onizuka-1999.jpg",
     category: "anime",
   },
   {
+    id: "gunsmith-cats",
     title: "Gunsmith Cats",
     poster: "/assets/images/animes/gunsmith-cats-1995.jpg",
     category: "anime",
   },
   {
+    id: "katekyou-hitman-reborn",
     title: "Katekyou Hitman Reborn!",
     poster: "/assets/images/animes/katekyou-hitman-reborn-2006.jpg",
     category: "anime",
   },
   {
+    id: "kill-la-kill",
     title: "Kill la Kill",
     poster: "/assets/images/animes/kill-la-kill-2013.jpg",
     category: "anime",
   },
   {
+    id: "little-witch-academia",
     title: "Little Witch Academia",
     poster: "/assets/images/animes/little-witch-academia-2017.jpg",
     category: "anime",
   },
   {
+    id: "perfect-blue",
     title: "Perfect Blue",
     poster: "/assets/images/animes/perfect-blue-1998.jpg",
     category: "anime",
   },
   {
+    id: "steins-gate",
     title: "Steins;Gate",
     poster: "/assets/images/animes/steins-gate-2011.jpg",
     category: "anime",
